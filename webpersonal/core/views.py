834d94b266a9045567d60824bcbca6241e1536a8
Diego Brito Sánchez <diegobritosanchez13@gmail.com>
2024-12-26 13:01:11 +0000

    a

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
 
-
 def about(request):
-    return render(request, "core/about-me.html")
-
-
-def portfolio(request):
-    return render(request, "core/portfolio.html")
-
+    return render(request, "core/about.html")
 
 def contact(request):
     return render(request, "core/contact.html")
+
